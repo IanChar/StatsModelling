b6bc8214896fc4b1cc0c3aae73b6f310facd6381
IanChar <ianchar4@gmail.com>
2017-04-06 15:51:28 -0600

    Created initial data loading in and cleaning

---
+++ LogisticModel.R
@@ -0,0 +1,41 @@
+### Script to generate logistic model of the data.
+TRAIN_PATH <- "data/train.json"
+NUM_SAMPLES = 100
+
+##############################DATA LOAD IN AND CLEANING############################
+# Load data in.
+# install.packages("rjson")
+library("rjson")
+library("jsonlite")
+data <- fromJSON(TRAIN_PATH)
+
+# Get useful predictors for this model and convert to doubles.
+cleaned <- data[c("bathrooms", "bedrooms", "price")]
+cleaned <- lapply(cleaned, function(lst) {
+  as.double(lst)
+})
+# Append response.
+interests <- as.double(lapply(data$interest_level, function(interest) {
+  toReturn <- -1
+  if (interest == "low") {
+    toReturn <- 0
+  } else if (interest == "medium") {
+    toReturn <- 1
+  } else if (interest == "high") {
+    toReturn <- 2
+  }
+  toReturn
+}))
+cleaned <- append(cleaned, list(interests))
+names(cleaned)[length(names(cleaned))] <- "interest_level"
+
+# Trim amount of data
+trimData <- function(dataset, size) {
+  lapply(dataset, function(elem) {
+    elem[1:size]
+  })
+}
+aptmts <- trimData(cleaned, NUM_SAMPLES)
+
+# Make cleaned into a Dataframe
+aptmts <- as.data.frame(cleaned)
