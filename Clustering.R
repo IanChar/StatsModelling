a5a83851c2232b1804b8bc3d4a59a938944a57a5
Andy Kim <andy.kim@workday.com>
2017-04-24 19:34:16 -0600

    Changed random forest

--- Clustering.R
+++ Clustering.R
@@ -1,11 +1,12 @@
 ### Script to generate logistic model of the data.
 set.seed(100)
 library(rpart)
+library(cluster)
 library(RColorBrewer)
 library(randomForest)
 ##############################DATA LOAD IN AND CLEANING############################
 # Load data in.
-source("load.R")
+#source("load.R")
 
 trainInd <- sample(1:dim(apt)[1], dim(apt)[1]*2/3)
 train <- apt[trainInd,]
@@ -15,18 +16,25 @@ test <- apt[-trainInd,]
 fit <- randomForest(as.factor(interest_level) ~ bathrooms + bedrooms + price + latitude + longitude
                                                 + num_photos + num_features + desc_length + month
                                                 + day + hour,
-                    data=apt, 
+                    data=train, 
                     importance=TRUE, 
-                    ntree=10)
+                    ntree=5)
+varImpPlot(fit)
 
-Prediction <- predict(fit, apt[,-12])
-tab <- cbind(unname(Prediction), apt[,12])
+Prediction <- predict(fit, test[,-12], type = "prob")
+Prediction <- apply(Prediction, c(1,2), function(x) min(max(x, 1E-15), 1-1E-15)) 
 
-mean(Prediction == apt[,12])
+LogLoss <- function(actual, predicted)
+{
+  result=-1/length(actual)*(sum((actual*log(predicted)+(1-actual)*log(1-predicted))))
+  return(result)
+}
+
+LogLoss(test[,12], Prediction[,3])
 
 
 numOfClust <- 3
-clustering <- clara(apt[,-c(1,2,3,6,7,8,9,10,11,12)], numOfClust)$clustering
+clustering <- clara(apt[,-c(1,2,3,6,7,8,9,10,11,12)], numOfClust, metric = "manhattan")$clustering
 
 color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
 colors <- sample(color, numOfClust)
