bc20e04e82c10c2c0f31a84233cbe77b64238fee
Andy Kim <jkim404330@gmail.com>
2017-04-29 11:24:47 -0600

    Changing clustering

--- Clustering.R
+++ Clustering.R
@@ -4,37 +4,16 @@ library(rpart)
 library(cluster)
 library(RColorBrewer)
 library(randomForest)
+library(foreach)
+library(doSNOW)
 ##############################DATA LOAD IN AND CLEANING############################
 # Load data in.
 #source("load.R")
 
-trainInd <- sample(1:dim(apt)[1], dim(apt)[1]*2/3)
-train <- apt[trainInd,]
-test <- apt[-trainInd,]
-
-
-fit <- randomForest(as.factor(interest_level) ~ bathrooms + bedrooms + price + latitude + longitude
-                                                + num_photos + num_features + desc_length + month
-                                                + day + hour,
-                    data=train, 
-                    importance=TRUE, 
-                    ntree=5)
-varImpPlot(fit)
-
-Prediction <- predict(fit, test[,-12], type = "prob")
-Prediction <- apply(Prediction, c(1,2), function(x) min(max(x, 1E-15), 1-1E-15)) 
-
-LogLoss <- function(actual, predicted)
-{
-  result=-1/length(actual)*(sum((actual*log(predicted)+(1-actual)*log(1-predicted))))
-  return(result)
-}
-
-LogLoss(test[,12], Prediction[,3])
-
-
-numOfClust <- 3
-clustering <- clara(apt[,-c(1,2,3,6,7,8,9,10,11,12)], numOfClust, metric = "manhattan")$clustering
+numOfClust <- 20
+numTree <- 100
+#clustering <- kmeans(apt[, c(4,5)], numOfClust, nstart = 100)$cluster
+clustering <- clara(apt[,c(4,5)], numOfClust, metric = "euclidean")$clustering
 
 color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
 colors <- sample(color, numOfClust)
@@ -42,4 +21,51 @@ colorClust <- colors[clustering]
 lon.lat <- cbind(apt$longitude, apt$latitude, colorClust)
 plot(lon.lat, cex = 0.5, pch = 16, col=colorClust)
 
+newApt <- apt
+newApt$clustering <- clustering 
 
+trainInd <- sample(1:dim(apt)[1], dim(apt)[1]*2/3)
+train <- apt[trainInd,]
+test <- apt[-trainInd,]
+
+fit <- randomForest(as.factor(interest_level) ~ bathrooms + bedrooms + price + latitude + longitude
+                    + num_photos + num_features + desc_length + month
+                    + day + hour,
+                    data=train, 
+                    importance=TRUE, 
+                    ntree=numTree)
+
+
+varImpPlot(fit)
+
+classPred <- predict(fit, test[,-12])
+Prediction <- predict(fit, test[,-12], type = "prob")
+Prediction <- apply(Prediction, c(1,2), function(x) min(max(x, 1E-15), 1-1E-15)) 
+mean(classPred == test[,12])
+
+# LogLoss <- function(actual, predicted)
+# {
+#   result=-1/length(actual)*(sum((actual*log(predicted)+(1-actual)*log(1-predicted))))
+#   return(result)
+# }
+# 
+# LogLoss(test[,12], Prediction[,3])
+
+trainInd <- sample(1:dim(newApt)[1], dim(newApt)[1]*2/3)
+train <- newApt[trainInd,]
+test <- newApt[-trainInd,]
+
+fit <- randomForest(as.factor(interest_level) ~ bathrooms + bedrooms + price + latitude + longitude
+                    + num_photos + num_features + desc_length + month
+                    + day + hour + clustering,
+                    data=train, 
+                    importance=TRUE, 
+                    ntree=numTree)
+
+
+varImpPlot(fit)
+
+classPred <- predict(fit, test[,-12])
+Prediction <- predict(fit, test[,-12], type = "prob")
+Prediction <- apply(Prediction, c(1,2), function(x) min(max(x, 1E-15), 1-1E-15)) 
+mean(classPred == test[,12])
